e5b05fe7761e277977be9dafd3db129207c56fbc
ryanhamley <ryan.hamley@mapbox.com>
2020-01-16 12:14:20 -0800

    Cache serialized layer rather than serializing on every feature

--- src/source/query_features.js
+++ src/source/query_features.js
@@ -40,6 +40,7 @@ function queryIncludes3DLayer(layers?: Array<string>, styleLayers: {[string]: St
 
 export function queryRenderedFeatures(sourceCache: SourceCache,
                             styleLayers: {[string]: StyleLayer},
+                            serializedLayers: {[string]: Object},
                             queryGeometry: Array<Point>,
                             params: { filter: FilterSpecification, layers: Array<string> },
                             transform: Transform) {
@@ -57,6 +58,7 @@ export function queryRenderedFeatures(sourceCache: SourceCache,
             wrappedTileID: tileIn.tileID.wrapped().key,
             queryResults: tileIn.tile.queryRenderedFeatures(
                 styleLayers,
+                serializedLayers,
                 sourceCache._state,
                 tileIn.queryGeometry,
                 tileIn.cameraQueryGeometry,
