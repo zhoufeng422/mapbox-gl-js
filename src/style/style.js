e106062a93b0d9ee5f8c6d44adef8d9b168c2f60
ryanhamley <ryan.hamley@mapbox.com>
2020-01-21 18:00:24 -0800

    Evaluate properties

--- src/style/style.js
+++ src/style/style.js
@@ -1082,10 +1082,7 @@ class Style extends Evented {
         for (const layer in this._layers) {
             serializedLayers[layer] = this._layers[layer].serialize();
         }
-        console.log('this._layers', this._layers);
-        console.log('serializedLayers', serializedLayers);
-        // console.log('serialized layer', this._layers['tracks'].serialize());
-        console.log('souceCaches', this.sourceCaches);
+
         for (const id in this.sourceCaches) {
             if (params.layers && !includedSources[id]) continue;
             sourceResults.push(
@@ -1113,8 +1110,6 @@ class Style extends Evented {
                     this.placement.retainedQueryData)
             );
         }
-
-        console.log('sourceResults', sourceResults);
 
         return this._flattenAndSortRenderedFeatures(sourceResults);
     }
